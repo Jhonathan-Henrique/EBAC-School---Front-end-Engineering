bb87fa14df5fffa30895c5ab7ce3520533831116
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-12-01 09:34:06 -0300

    chore: add first project grunt

--- Grunt/configurando o grunt/gruntfile.js
+++ Grunt/configurando o grunt/gruntfile.js
@@ -1,5 +1,47 @@
-MediaSourceHandle.exports = function(grunt){
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
-  })
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		less: {
+			development: { //Essa devepmen podemos esta criando configuraçoes diferentes
+			//A partir do ambiente onde o grunt e executado
+				files: {
+					'main.css': 'main.less'
+				}
+			},
+			production: {
+				options: {
+					compress: true
+				},
+				files: {
+					'main.min.css': "main.less"
+				}
+			}
+		},
+		sass: {
+			dist: {
+				options: {
+				style: 'compressed'
+				},
+				files: {
+					'main2.css': 'main.scss'
+				}
+			}
+		}
+	})
+	
+	grunt.registerTask('olaGrunt', function(){
+	
+		const done = this.async();
+		setTimeout(function(){
+			console.log('Olá Grunt!');
+			done();
+		}, 3000)
+		
+	})
+	
+	grunt.loadNpmTask('grunt-contrib-less');
+	grunt.loadNpmTask('grunt-contrib-sass');
+	
+	grunt.registerTask('default', ['less', 'sass']) // Aqui podemos um array do nome de todas
+	//as tarfeas que serao executadas quando escrevermos npm run grunt
 }
